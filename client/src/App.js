e95d0849fde7b029cab8982d9024ef96e57f6096
Teo Mastro <teomastro1999@gmail.com>
2022-11-26 16:02:09 +0200

    user widget added

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  // const isAuth = Boolean(useSelector((state) => state.token));
-  const isAuth = true;
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
